3c773e35f3f8ee4ed0090d71ef8161871195379c
Hazem <hazem77mostafa90@gmail.com>
2026-05-21 16:18:33 +0300

    implement getByText

--- src/components/Application/TheRestOfGetByApplication.test.tsx
+++ src/components/Application/TheRestOfGetByApplication.test.tsx
@@ -2,16 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { Application } from "./Application";
 
-describe("Application getByPlaceholderText", () => {
-  test("getByLabelText", () => {
+describe("Application the rest of get by query", () => {
+  test("let's test", () => {
     render(<Application />);
     const Fullname = screen.getByPlaceholderText("Fullname");
     expect(Fullname).toBeInTheDocument();
 
     const paragraphElement = screen.getByText("All fields are mandatory");
     expect(paragraphElement).toBeInTheDocument();
-
-    const nameElement = screen.getByDisplayValue("Hazem");
-    expect(nameElement).toBeInTheDocument();
   });
 });
