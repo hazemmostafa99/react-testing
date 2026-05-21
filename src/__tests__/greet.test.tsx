e2de362281e52794362a79dfb42dd1b55c789645
Hazem <hazem77mostafa90@gmail.com>
2026-05-21 12:16:00 +0300

    Filename Conventions

--- src/__tests__/greet.test.tsx
+++ src/__tests__/greet.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import Greet from "./Greet";
+import Greet from "../components/Greet";
 
 describe("Greet", () => {
   // fit to focus on this test === only
