352b10f6cf966b4c6aabd7fa646ad4d223735106
Hazem <hazem77mostafa90@gmail.com>
2026-05-22 12:21:24 +0300

    test Custom React Hooks

---
+++ src/hooks/useCounter.test.tsx
@@ -0,0 +1,19 @@
+import { renderHook } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { useCounter } from "./useCounter";
+
+describe("useCounter", () => {
+  test("should render the initial count", () => {
+    const { result } = renderHook(useCounter);
+    expect(result.current.count).toBe(0);
+  });
+
+  test("should accept render the same initial count", () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: {
+        initialCount: 10,
+      },
+    });
+    expect(result.current.count).toBe(10);
+  });
+});
