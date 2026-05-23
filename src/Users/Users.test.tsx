6970954da0a6dca00e809eb7c69a4e9d5228ae23
Hazem <hazem77mostafa90@gmail.com>
2026-05-23 08:31:49 +0300

    MSW Error Handling

--- src/Users/Users.test.tsx
+++ src/Users/Users.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { Users } from "./Users";
+import { server } from "../mocks/server";
+import { http, HttpResponse } from "msw";
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -13,5 +15,16 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("renders an error", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/users", () => {
+        return new HttpResponse({ status: 500 });
+      }),
+    );
+    render(<Users />);
+    const erorr = await screen.findByText("Error fetching users");
+    expect(erorr).toBeInTheDocument();
   });
 });
